708ccba38593b15ba0255215389288517d6b6111
Jaco Horn (Home) <jacohorn1@gmail.com>
2019-04-02 21:21:33 +0200

    Added sound for metronome.

--- js/metronome.js
+++ js/metronome.js
@@ -1,6 +1,7 @@
 var running = false;
+const sound = new Audio();
 
-function startMetronome () {
+function startMetronome() {
   document.getElementById("StartButton").disabled = true;
   document.getElementById("StopButton").disabled = false;
   document.getElementById("bpm").disabled = true;
@@ -9,23 +10,24 @@ function startMetronome () {
   metronomeLoop(bps);
 }
 
-function stopMetronome () {
+function stopMetronome() {
   document.getElementById("StartButton").disabled = false;
   document.getElementById("StopButton").disabled = true;
   document.getElementById("bpm").disabled = false;
   running = false;
 }
 
-function metronomeLoop (bps) {
- setTimeout(function () {
+function metronomeLoop(bps) {
+  setTimeout(function() {
     if (running == true) {
-      document.getElementById("mTest").innerHTML = document.getElementById("mTest").innerHTML + 1;
+      sound.src = './media/Recording.m4a';
+      sound.play();
       metronomeLoop(bps);
     }
- }, bps)
+  }, bps)
 }
 
-function beatsPerSecond (beatsPM) {
+function beatsPerSecond(beatsPM) {
   var bps = beatsPM / 60;
   var result = 1000 / bps;
   return result;
